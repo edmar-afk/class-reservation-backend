3ffcfbe3759f5ab8b9636b06b5ecf6636d88ba3d
edmar-afk <jaywrsnp6@gmail.com>
2026-04-23 17:34:16 +0800

    new

--- api/serializers.py
+++ api/serializers.py
@@ -6,7 +6,7 @@ from .models import Instructor, Student, Subject, Room, Reservation
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "email", "first_name", "last_name"]
+        fields = ["id", "username", "email", "first_name", "last_name", 'is_superuser']
 
 
 class StudentSerializer(serializers.ModelSerializer):
